3588c03c3a0ef65d2eb10069eba23c06e5e00360
jasperbonte <105388419+jasperbonte@users.noreply.github.com>
2023-01-22 13:02:34 +0100

    Update matrix.py
    fastened algoritms

--- matrix.py
+++ matrix.py
@@ -1,34 +1,35 @@
-"a matrix library"
-
-from copy import deepcopy
-from typing import Union
+from fractions import Fraction
 
 
 class Matrix:
     """Matrix Class"""
 
-    def __init__(self, elements: list[list[float]]):
+    def __init__(self, elements: list[list[float | Fraction]]):
         """Creates a matrix
 
         Args:
-            elements (list[list[float]]): elements grouped by row
+            elements (list[list[float | Fraction]]): elements grouped by row
         """
         self.elements = elements
+        self.rows = len(self.elements)
+        self.columns = len(self.elements[0])
+        self.dimension = self.rows, self.columns
 
-    def __getitem__(self, place: tuple[int, int]) -> float:
-        row, column = place
-        return self.elements[row][column]
+    def __getitem__(self, place: tuple[int, int]) -> float | Fraction:
+        return self.elements[place[0]][place[1]]
 
-    def __setitem__(self, place: tuple[int, int], val: float) -> None:
+    def __setitem__(self, place: tuple[int, int], val: float | Fraction) -> None:
         self.elements[place[0]][place[1]] = val
 
     def __str__(self) -> str:
-        return str(self.elements).replace("[[", "[").replace("[", "\n[")
+        return "[{}]".format(
+            "\n ".join(" ".join(str(j) for j in i) for i in self.elements)
+        )
 
     def __eq__(self, other: object) -> bool:
         if isinstance(other, Matrix):
             return self.elements == other.elements
-        return False
+        raise ValueError(f"can't compare matrix to {type(other)}")
 
     def copy(self) -> "Matrix":
         """Copys the matrix
@@ -36,91 +37,53 @@ class Matrix:
         Returns:
             Matrix: the copyed matrix
         """
-        return Matrix(deepcopy(self.elements))
-
-    def get_dimension(self) -> tuple[int, int]:
-        """Get dimensions of matrix
-
-        Returns:
-            tuple[int, int]: rows, columns
-        """
-        return self.get_rows(), self.get_columns()
-
-    def get_rows(self) -> int:
-        """Get amount of rows
-
-        Returns:
-            int: amount rows
-        """
-        return len(self.elements)
-
-    def get_columns(self) -> int:
-        """Get amount of columns
-
-        Returns:
-            int: amount columns
-        """
-        return len(self.elements[0])
+        return Matrix([[val for val in row] for row in self.elements])
 
     def __add__(self, other: "Matrix") -> "Matrix":
         return Matrix(
             [
-                [self[r, c] + other[r, c] for c in range(self.get_columns())]
-                for r in range(self.get_rows())
+                [a + b for a, b in zip(row1, row2)]
+                for row1, row2 in zip(self.elements, other.elements)
             ]
         )
 
     def __sub__(self, other: "Matrix") -> "Matrix":
         return Matrix(
             [
-                [self[r, c] - other[r, c] for c in range(self.get_columns())]
-                for r in range(self.get_rows())
+                [a - b for a, b in zip(row1, row2)]
+                for row1, row2 in zip(self.elements, other.elements)
             ]
         )
 
-    def __mul__(self, other: Union["Matrix", float]) -> "Matrix":
+    def __mul__(self, other: "Matrix | float | Fraction") -> "Matrix":
         if isinstance(other, Matrix):
             return Matrix(
                 [
                     [
-                        sum(
-                            [
-                                self[r, i] * other[i, c]
-                                for i in range(self.get_columns())
-                            ]
-                        )
-                        for c in range(other.get_columns())
+                        sum(a * b for a, b in zip(row1, col2))
+                        for col2 in zip(*other.elements)
                     ]
-                    for r in range(self.get_rows())
+                    for row1 in self.elements
                 ]
             )
-        return Matrix(
-            [
-                [other * self[r, c] for c in range(self.get_columns())]
-                for r in range(self.get_rows())
-            ]
-        )
+        return Matrix([[val * other for val in row] for row in self.elements])
 
     def __pow__(self, exp: int) -> "Matrix":
-        res = IdentityMatrix(self.get_rows())
+        if exp == -1:
+            return self.invers()
+        res = IdentityMatrix(self.rows)
         for _ in range(exp):
             res *= self
         return res
 
     def transpose(self) -> "Matrix":
         """Transpose a matrix
+        does not change the original matrix
 
         Returns:
             Matrix: the transposed matrix
         """
-        res = Matrix(
-            [[0 for _ in range(self.get_rows())] for _ in range(self.get_columns())]
-        )
-        for r in range(self.get_rows()):
-            for c in range(self.get_columns()):
-                res[c, r] = self[r, c]
-
-        return res
+        return Matrix(list(map(list, zip(*self.elements))))
 
     def rowswap(self, a: int, b: int) -> None:
         """Swap two rows
@@ -133,7 +96,7 @@ class Matrix:
         self.elements[a] = self.elements[b]
         self.elements[b] = r_a
 
-    def rowmult(self, r: int, n: float) -> None:
+    def rowmult(self, r: int, n: float | Fraction) -> None:
         """Multiply a row with a float
 
         Args:
@@ -153,48 +116,93 @@ class Matrix:
             self.elements[r1][n] + self.elements[r2][n] for n in range(r1)
         ]
 
-    def spill(self) -> None:
-        """Spill the matrix"""
-        for i in range(self.get_rows()):
-            print(self)
-            spil = self[i, i]
-            if spil == 0:
-                for j in range(self.get_rows())[1:]:
+    def rref(self) -> None:
+        """Spill the matrix
+        Original matrix gets affected"""
+        for i in range(self.rows):
+            spil = self[i, i]  # set spill
+            if spil == 0:  # spill can't be zero
+                for j in range(self.rows)[1:]:
                     if self[j, j] != 0:
                         self.rowswap(i, j)
-                        self.spill()
-                    return
+                        spil = self[i, i]  # change the spill
+                        break
+                    else:
+                        return
 
             # add zeros
             pr_matrix = self.copy()
-            for j in range(self.get_rows()):
+            for j in range(self.rows):
                 if j != i:
                     self[j, i] = 0
 
             # kruisproducten
-            for r in range(self.get_rows()):
-                for c in range(self.get_columns()):
-                    if r == i or c <= i:
-                        continue
-                    else:
+            for r in range(self.rows):
+                if r != i:
+                    for c in range(self.columns):
                         self[r, c] = (spil * pr_matrix[r, c]) - (
                             pr_matrix[i, c] * pr_matrix[r, i]
                         )
+            for i in range(self.rows):
+                a = self[i, i]
+                f = 10 ** (len(str(a)) - str(a).count("."))
+                b = self[i, i] * f
 
-    def inverse(self) -> None:
-        """Inverse the matrix"""
-        l: list[list[float]] = []
-        for i in range(self.get_rows()):
+                for j in range(self.columns):
+                    self[i, j] = Fraction(self[i, j] * f, b)
+
+    def invers(self) -> "Matrix":
+        """Inverses a Matrix
+        original matrix does not change
+
+        Raises:
+            ValueError: can't inverse matrix
+
+        Returns:
+            Matrix: the inversed matrix
+        """
+        if self.det() == 0 or self.rows != self.columns:
+            raise ValueError("Can't inverse matrix")
+
+        l: list[list[float | Fraction]] = []
+        for i in range(self.rows):
             l.append([])
             l[-1].extend(self.elements[i])
-            l[-1].extend([1 if i == j else 0 for j in range(self.get_rows())])
-        self.elements = l
-        self.spill()
+            l[-1].extend([1 if i == j else 0 for j in range(self.rows)])
+        a = Matrix(l)
+        a.rref()
+        b: list[list[float | Fraction]] = []
+        for r in range(a.rows):
+            b.append([])
+            for c in range(a.columns)[self.rows :]:
+                b[-1].append(a[r, c])
+        return Matrix(b)
+
+    def det(self) -> float | Fraction:
+        if self.rows != self.columns:
+            raise ValueError(f"Cant find det of {self.rows} x {self.columns} matrix")
+        if self.rows == 1:
+            return self[0, 0]
+        elif self.rows == 2:
+            return self[0, 0] * self[1, 1] - self[1, 0] * self[0, 1]
+        else:
+            # ontwikkelen naar rij 1
+            s = 0
+            for i in range(self.columns):
+                l: list[list[float | Fraction]] = []
+                for r in range(self.rows):
+                    if r != 1:
+                        l.append([])
+                        for c in range(self.columns):
+                            if c != i:
+                                l[-1].append(self[r, c])
+                s += (1 if i % 2 == 1 else -1) * self[1, i] * Matrix(l).det()
+            return s
 
 
 class IdentityMatrix(Matrix):
     def __init__(self, rows: int):
-        res: list[list[float]] = [
+        res: list[list[float | Fraction]] = [
             [1 if i == j else 0 for i in range(rows)] for j in range(rows)
         ]
         super().__init__(res)
